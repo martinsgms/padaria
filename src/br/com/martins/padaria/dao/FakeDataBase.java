a4e789f72157e48177f48b2fc1c06f90baa7a9c3
Gabriel Martins <martinsgms21@gmail.com>
2020-04-21 22:51:49 -0300

    cria banco de dados fake

---
+++ src/br/com/martins/padaria/dao/FakeDataBase.java
@@ -0,0 +1,19 @@
+package br.com.martins.padaria.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.martins.padaria.model.Cliente;
+
+public class FakeDataBase {
+    
+    private static List<Cliente> clientes = new ArrayList<Cliente>();
+    private Integer idGenerator = 0;
+    
+    public void create(Cliente cliente) {
+        
+        cliente.setId(idGenerator++);
+        clientes.add(cliente);
+    }
+    
+}
